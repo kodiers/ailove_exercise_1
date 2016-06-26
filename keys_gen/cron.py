30c72a7f560af44599ceaeec2f23c1da828afa18
Viktor Yamchinov <kodiers@gmail.com>
2016-06-27 01:15:12 +0300

    Added check keys count

--- keys_gen/cron.py
+++ keys_gen/cron.py
@@ -1,6 +1,7 @@
 import kronos
 
 from .functions import create_random_key
+from .models import Keys
 
 
 @kronos.register('01 0 * * *')
@@ -8,6 +9,7 @@ def generate_keys():
     """
     Cron task used to generate keys at 0:0 o'clock every day.
     """
-    for i in range(100):
-        create_random_key()
+    if Keys.objects.filter(status=0).count() < 10:
+        for i in range(100):
+            create_random_key()
 
